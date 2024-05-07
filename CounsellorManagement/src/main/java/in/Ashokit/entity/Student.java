95d4c7aa4f25289339314227ee8742cc347cf8b4
chandu00319 <you@example.com>
2024-05-07 13:13:39 +0530

    servers

--- CounsellorManagement/src/main/java/in/Ashokit/entity/Student.java
+++ CounsellorManagement/src/main/java/in/Ashokit/entity/Student.java
@@ -1,5 +1,21 @@
 package in.Ashokit.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Entity
+@Data
 public class Student {
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String fname;
+	private String lname;
+	private Long phno; 
+	private String email;
+	
+	
 }
---
+++ CounsellorManagement/src/main/java/in/Ashokit/repo/Administratorrepo.java
@@ -0,0 +1,9 @@
+package in.Ashokit.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.Ashokit.entity.Administrator;
+
+public interface Administratorrepo extends JpaRepository<Administrator, Long> {
+
+}
